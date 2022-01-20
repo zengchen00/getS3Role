a7f4034e2695e8d3e9b6b19dde05f318c557e047
zengchen <zengchen@kingsoft.com>
2022-01-20 10:25:41 +0800

    s3 objects list

--- src/main/java/S3Operation.java
+++ src/main/java/S3Operation.java
@@ -1,5 +1,29 @@
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.List;
+
+
 public class S3Operation {
     public static void main(String[] args) {
+        var s3Client = S3Client.builder().region(Region.of("cn-northwest-1")).build();
+
+        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request
+                .builder()
+                .bucket("test-ad")
+                .build();
+        ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsRequest);
+
+        System.out.println("keyCount:" + listObjectsV2Response.keyCount());
+        List<S3Object> contents = listObjectsV2Response.contents();
+        if (contents != null) {
+            for (S3Object content : contents) {
+                System.out.println("object key" + content.key());
+            }
+        }
 
     }
 }
