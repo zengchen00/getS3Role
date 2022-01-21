7b658a39e89c7de90ee4cc9244152481647cfb45
zengchen00 <zengchen@kingsoft.com>
2022-01-21 12:11:03 +0800

    add sdk v1

--- src/main/java/S3Operation.java
+++ src/main/java/S3Operation.java
@@ -1,3 +1,7 @@
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
@@ -9,21 +13,33 @@ import java.util.List;
 
 public class S3Operation {
     public static void main(String[] args) {
-        var s3Client = S3Client.builder().region(Region.of("cn-northwest-1")).build();
+        String bucket_name = "test-ad";
 
-        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request
-                .builder()
-                .bucket("test-ad")
-                .build();
-        ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsRequest);
-
-        System.out.println("keyCount: " + listObjectsV2Response.keyCount());
-        List<S3Object> contents = listObjectsV2Response.contents();
-        if (contents != null) {
-            for (S3Object content : contents) {
-                System.out.println("object key: " + content.key());
+        System.out.println("*********** sdk v1 ***********");
+        AmazonS3 amazonS3 = AmazonS3Client.builder().withRegion("cn-northwest-1").build();
+        ListObjectsV2Result result = amazonS3.listObjectsV2(bucket_name);
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+        if(null != objects){
+            System.out.println("v1 keyCount: " + objects.size());
+            for (S3ObjectSummary os : objects) {
+                System.out.println("v1 object_key: " + os.getKey());
             }
         }
 
+
+        System.out.println("*********** sdk v2 ***********");
+        S3Client s3Client = S3Client.builder().region(Region.of("cn-northwest-1")).build();
+        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request
+                .builder()
+                .bucket(bucket_name)
+                .build();
+        ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsRequest);
+        System.out.println("v2 keyCount: " + listObjectsV2Response.keyCount());
+        List<S3Object> contents = listObjectsV2Response.contents();
+        if (contents != null) {
+            for (S3Object content : contents) {
+                System.out.println("v2 object_key: " + content.key());
+            }
+        }
     }
 }
